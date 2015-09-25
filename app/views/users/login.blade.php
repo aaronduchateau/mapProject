6314929a852f204fe427508106d6ee1089f7be2e
Aaron DuChateau <chateauconcept@gmail.com>
2015-09-24 17:35:16 -0700

    log prod env

--- app/views/users/login.blade.php
+++ app/views/users/login.blade.php
@@ -1,3 +1,4 @@
+<div style="display:none;">{{App::environment()}}</div>
 <div class="sign-in-div">
 	{{ Form::open(array('url'=>'users/signin')) }}
 		<h4 class="form-signin-heading" style="color:white;">Please Login</h4>
